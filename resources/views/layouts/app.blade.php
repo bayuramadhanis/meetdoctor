ee077339d6b3d6c856e545c7b6a39d26dc8ccb8d
Bayu Ramadhanis <you@example.com>
2022-10-11 16:19:52 +0700

    Change vite to mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,12 +9,15 @@
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Styles -->
         @livewireStyles
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!--@vite(['resources/css/app.css', 'resources/js/app.js'])-->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+        
     </head>
     <body class="font-sans antialiased">
         <x-jet-banner />
