37f27dc9edd26b9c565c5820f82edddf2c64c7ac
Bayu Ramadhanis <you@example.com>
2022-10-19 14:52:02 +0700

    models seed done

---
+++ database/seeders/ConfigPaymentSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use App\Models\MasterData\ConfigPayment;
+
+use Illuminate\Database\Seeder;
+use illuminate\Support\Facades\DB;
+
+class ConfigPaymentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $config_payment = [
+            [
+                'fee' => '150000',
+                'vat' => '20', //vat is percentage
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        //this array specialist will be insert to table
+        ConfigPayment::insert($config_payment);
+    }
+}
---
+++ database/seeders/SpecialistSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\MasterData\Specialist;
+
+use Illuminate\Database\Seeder;
+use illuminate\Support\Facades\DB;
+
+class SpecialistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //create data here
+        $specialist = [
+            [
+                'name' => 'Dermatology',
+                'price' => '250000',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Neurology',
+                'price' => '100000',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'History',
+                'price' => '50000',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Science',
+                'price' => '75000',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Music',
+                'price' => '25000',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'name' => 'Art',
+                'price' => '25000',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
+
+        //this array specialist will be insert to table
+        Specialist::insert($specialist);
+    }
+}
